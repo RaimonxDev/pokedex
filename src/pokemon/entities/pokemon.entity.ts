b460c475ac675b03440cfc8280e20139de5d5f60
Ramon Martinez Work <ramonmartinezcordero@gmail.com>
2022-09-05 19:32:08 -0300

    first schema pokemon

--- src/pokemon/entities/pokemon.entity.ts
+++ src/pokemon/entities/pokemon.entity.ts
@@ -1,1 +1,20 @@
-export class Pokemon {}
+import { Schema, SchemaFactory, Prop } from "@nestjs/mongoose";
+import { Document } from "mongoose";
+
+@Schema()
+export class Pokemon extends Document {
+  @Prop({
+    unique: true,
+    index: true
+  })
+  no: number;
+
+  @Prop({
+    unique: true,
+    index: true
+  })
+  name: string;
+}
+
+
+export const PokemonSchema = SchemaFactory.createForClass(Pokemon)
